a56232863e84974c23d09d68e1ae70cc77d5081c
Anthony Nassar <ajnassar@gmail.com>
2014-01-15 11:56:42 -0800

    Created Session controller and parts of its functionality. Updated Routes shirts index is now shirts create, added session create destroy and new.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 TeeFuryClone::Application.routes.draw do
   get "static_pages/home"
 
+  resource :session, :only => [:create, :destroy, :new]
   resources :users, :only => [:index, :create, :new, :show]
-  resources :shirts, :only => [:index]
+  resources :shirts, :only => [:create]
 
   root :to => "static_pages#home"
 end
