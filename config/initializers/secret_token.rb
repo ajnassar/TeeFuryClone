5c2838ffd085df69a9dcf48b35947747db161b7d
App Academy Student <student@appacademy.io>
2014-01-15 09:24:51 -0800

    Initial push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TeeFuryClone::Application.config.secret_token = '10055e2c2ac5331423cc72d3b40166c6a5d6f2b1b53ad89cdf298a8f6ba9ab5f7c257bf63805805c3e43316581d5adb52106a018cb0874a5e9a2f7007b67531e'
