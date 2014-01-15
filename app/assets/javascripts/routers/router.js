ab95f0d2ed51e1504f46d9118d4bf96225ce2cd1
Anthony Nassar <ajnassar@gmail.com>
2014-01-15 12:10:58 -0800

    Added showShirt route to router.js

--- app/assets/javascripts/routers/router.js
+++ app/assets/javascripts/routers/router.js
@@ -6,18 +6,21 @@ TeeFuryClone.Routers.Router = Backbone.Router.extend(
 		routes:{
 			"":"TodaysTee"
 			"gallery":"galleryIndex",
-			"submit":"submitIndex"
+			"submit":"submitIndex",
+			"gallery/:id/":"showShirt"
 		},
 		TodaysTee: function(){
-
+			//shirt model
 		},
 		galleryIndex: function(){
-
+			//collection of shirts
 		},
 		submitIndex: function(){
 
 		},
+		showShirt: function(id){
 
+		},
 		_swapView: function(view){
 			this._currentView && this._currentView.remove();
 			this._currentView = view;
