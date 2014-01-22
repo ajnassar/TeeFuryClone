8cf0c3a468dab69531e851c3d21b6a947d135433
Anthony Nassar <ajnassar@gmail.com>
2014-01-22 14:17:06 -0800

    created seed file to upload multiple shirts

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+#create 10 users here
+
+Dir.chdir('images')
+Dir.foreach('.') { |filename|
+  next unless filename.include?('.png')
+  shirt_name = /([^-]*).png$/.match(filename)[1]
+  shirt = Shirt.new({ :name => shirt_name, :artist_id => 1, :submitted => false, color: "#FFF"  })
+  file = File.open("/Users/appacademy/Desktop/TeeFuryClone/images/#{filename}")
+  shirt.design_img = file
+  shirt.save!
+  file.close()
+}
