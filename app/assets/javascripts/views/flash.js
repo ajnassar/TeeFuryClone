0262108f372186a9945253edb0d4745995e9a8e5
Anthony Nassar <ajnassar@gmail.com>
2014-01-31 23:07:17 -0800

    Added close button to flash messages

--- app/assets/javascripts/views/flash.js
+++ app/assets/javascripts/views/flash.js
@@ -2,12 +2,15 @@ TeeFuryClone.Views.FlashMessage = Backbone.View.extend({
     initialize: function() {
        this.listenTo(TeeFuryClone.Dispatcher, 'show_flash_message', this.render);
     },
+    template: JST['flash_message'],
     render: function(msg) {
+    	var renderedContent = this.template({ meassage: msg }) + msg;
     	if (msg == "") {
 				$('#flash_message').css("display","none");
     	}else {
     		$('#flash_message').css("display","block");
     	}
-			$('#flash_message').text(msg);
+			$('#flash_message').html(renderedContent);
+
     }
 });
